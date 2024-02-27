b06f9c2626f518ff008f92122d4cd01e497178c2
jess <99680291+jessy3008@users.noreply.github.com>
2024-02-27 16:03:23 -0300

    add arquivo

---
+++ aula 27.02/sinatxe.js
@@ -0,0 +1,29 @@
+let nome; // valor manipulável
+let idade;
+
+nome = "Jess"; // string
+idade = 19; // number
+
+const altura = 1.62; // (CONST) valor constante
+
+let bol = true /// boolean
+let lista = ['banana', 'pera','uva']; // lista
+let objeto = {nome:"elysson", idade: 18}; // objeto
+
+
+
+
+//tenho que pegar essa bosta 
+
+
+
+
+
+
+let resto = a % b;
+
+// operadores de atribuição
+let x = 10;
+
+x += 5 // 
+x += 3
